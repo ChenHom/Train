f35c53649da44c4aa62e2f1c242e4b3ff15c5b44
Hom Chen <homchen@Homde-MacBook-Pro.local>
2019-01-31 17:29:00 +0800

    增加 c# 版跟 ignore 檔

---
+++ algorithm/example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var method1 = new Program();
+            Console.WriteLine(method1.factorial(10));
+        }
+
+        int factorial(int numeric)
+        {
+            int tmp = numeric - 1;
+
+            if (tmp == 1)
+            {
+                return numeric;
+            }
+            else
+            {
+                return this.factorial(tmp) * numeric;
+            }
+        }
+    }
+}
